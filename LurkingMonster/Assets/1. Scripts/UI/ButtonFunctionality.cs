ce3a53f1edae4ec6b74a1b1d15cc6718f9093222
Danny van Duist <dvduist@hotmail.nl>
2020-09-11 15:07:43 +0200

    Add a placeholder mainmenu.

---
+++ LurkingMonster/Assets/1. Scripts/UI/ButtonFunctionality.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using VDFramework;
+
+namespace UI
+{
+    public class ButtonFunctionality : BetterMonoBehaviour
+    {
+        public void QuitApplication()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+#else
+			UnityEngine.Application.Quit();
+#endif
+        }
+
+        public void LoadScene(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        public void LoadScene(int buildIndex)
+        {
+            SceneManager.LoadScene(buildIndex);
+        }
+    }
+}
