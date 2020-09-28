b263221b4190f93bf37feb30be094e21086efd38
Danny van Duist <dvduist@hotmail.nl>
2020-09-28 12:27:17 +0200

    Made the rent be a percentage of the building price

--- LurkingMonster/Assets/1. Scripts/ScriptableObjects/BuildingTypeData.cs
+++ LurkingMonster/Assets/1. Scripts/ScriptableObjects/BuildingTypeData.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 
 namespace ScriptableObjects
 {
-    [CreateAssetMenu(menuName = "BuildingType Data")]
-    public class BuildingTypeData : ScriptableObject
-    {
-        [SerializeField]
-        private int rent = 10;
+	[CreateAssetMenu(menuName = "BuildingType Data")]
+	public class BuildingTypeData : ScriptableObject
+	{
+		[SerializeField, Tooltip("The percentage of the price that will be collected as rent")]
+		private int rentPercentage = 30;
 
-        [SerializeField]
-        private int weight = 100;
+		[SerializeField]
+		private int weight = 100;
 
-        [SerializeField]
-        private int price = 10;
+		[SerializeField]
+		private int price = 10;
 
-        public BuildingData GetStruct()
-        {
-            return new BuildingData(rent, weight, price, default, default);
-        }
-    }
+		public BuildingData GetStruct()
+		{
+			return new BuildingData((int) (rentPercentage / 100.0f * price), weight, price, default, default);
+		}
+	}
 }
