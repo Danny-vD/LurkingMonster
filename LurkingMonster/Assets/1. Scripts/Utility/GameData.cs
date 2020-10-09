5e36d1a96a32498dedf8474e65dbc259e835ac12
NiekTempert <niektempert@gmail.com>
2020-10-09 12:38:18 +0200

    Worked on saving data to file, and reloading data on start game

---
+++ LurkingMonster/Assets/1. Scripts/Utility/GameData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Utility
+{
+	[Serializable]
+	public class GameData
+	{
+		private string cityName;
+		private string userName;
+
+		public GameData(string cityName, string userName)
+		{
+			this.cityName = cityName;
+			this.userName = userName;
+		}
+
+		public string CityName
+		{
+			get => cityName;
+			set => cityName = value;
+		}
+
+		public string UserName
+		{
+			get => userName;
+			set => userName = value;
+		}
+	}
+}
