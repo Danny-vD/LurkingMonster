44f79f227397be9a8267dd4fb5c500979f05649d
Danny-vD <dvduist@hotmail.nl>
2020-11-06 16:19:08 +0100

    Refactor code so that all health is being handled through the BuildingHealth component.

--- LurkingMonster/Assets/1. Scripts/Gameplay/Buildings/BuildingHealth.cs
+++ LurkingMonster/Assets/1. Scripts/Gameplay/Buildings/BuildingHealth.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using VDFramework;
+﻿using VDFramework;
 
 namespace Gameplay.Buildings
 {
@@ -8,36 +7,89 @@ namespace Gameplay.Buildings
 	/// </summary>
 	public class BuildingHealth : BetterMonoBehaviour
 	{
-		private float maxSoilHealth;
-		private float maxFoundationHealth;
-		private float maxBuildingHealth;
+		public float MaxSoilHealth { get; private set; }
+		public float MaxFoundationHealth { get; private set; }
+		public float MaxBuildingHealth { get; private set; }
 
-		private float soilHealth;
-		private float foundationHealth;
-		private float buildingHealth;
+		public float CurrentSoilHealth { get; private set; }
+		public float CurrentFoundationHealth { get; private set; }
+		public float CurrentBuildingHealth { get; private set; }
+
+		public float TotalHealth => CurrentSoilHealth + CurrentFoundationHealth + CurrentBuildingHealth;
+		public float MaxTotalHealth => MaxSoilHealth + MaxFoundationHealth + MaxBuildingHealth;
 
 		public void Initialize(float newMaxSoilHealth, float newMaxFoundationHealth, float newMaxBuildingHealth)
 		{
 			SetMaxSoilHealth(newMaxSoilHealth);
 			SetMaxFoundationHealth(newMaxFoundationHealth);
 			SetMaxBuildingHealth(newMaxBuildingHealth);
+
+			ResetHealth();
+		}
+
+		public void SetCurrentHealth(float soilHealth, float foundationHealth, float buildingHealth)
+		{
+			CurrentSoilHealth       = soilHealth;
+			CurrentFoundationHealth = foundationHealth;
+			CurrentBuildingHealth   = buildingHealth;
+		}
+
+		public void ResetHealth()
+		{
+			ResetSoilHealth();
+			ResetFoundationHealth();
+			ResetBuildingHealth();
+		}
+
+		public bool DamageSoil(float damage)
+		{
+			CurrentSoilHealth -= damage;
+
+			return CurrentSoilHealth <= 0;
+		}
+
+		public bool DamageFoundation(float damage)
+		{
+			CurrentFoundationHealth -= damage;
+
+			return CurrentFoundationHealth <= 0;
+		}
+
+		public bool DamageBuilding(float damage)
+		{
+			CurrentBuildingHealth -= damage;
+
+			return CurrentBuildingHealth <= 0;
 		}
 
 		public void SetMaxSoilHealth(float maxHealth)
 		{
-			maxSoilHealth = maxHealth;
+			MaxSoilHealth = maxHealth;
 		}
 
 		public void SetMaxFoundationHealth(float maxHealth)
 		{
-			maxFoundationHealth = maxHealth;
+			MaxFoundationHealth = maxHealth;
 		}
 
 		public void SetMaxBuildingHealth(float maxHealth)
 		{
-			maxBuildingHealth = maxHealth;
+			MaxBuildingHealth = maxHealth;
 		}
-		
-		
+
+		public void ResetSoilHealth()
+		{
+			CurrentSoilHealth = MaxSoilHealth;
+		}
+
+		public void ResetFoundationHealth()
+		{
+			CurrentFoundationHealth = MaxFoundationHealth;
+		}
+
+		public void ResetBuildingHealth()
+		{
+			CurrentBuildingHealth = MaxBuildingHealth;
+		}
 	}
 }
