f3fe9282b9601131423518c20255c6cbdb53e030
Danny-vD <dvduist@hotmail.nl>
2020-11-13 15:42:10 +0100

    Made the KCAF button open the KCAF webpage

---
+++ LurkingMonster/Assets/1. Scripts/UI/Buttons/Utility/ButtonURLOpener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+using VDFramework;
+
+namespace UI.Buttons.Utility
+{
+	[RequireComponent(typeof(URLOpener), typeof(Button))]
+	public class ButtonURLOpener : BetterMonoBehaviour
+	{
+		private URLOpener urlOpener;
+
+		private void Awake()
+		{
+			urlOpener = GetComponent<URLOpener>();
+			
+			GetComponent<Button>().onClick.AddListener(urlOpener.OpenURL);
+		}
+	}
+}
