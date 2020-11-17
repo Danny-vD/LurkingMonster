803ea7c21ab8e6e14301b924c1a0809bdcc52bae
NiekTempert <niektempert@gmail.com>
2020-11-17 14:25:45 +0100

    Added script for changing sprites for buttons

---
+++ LurkingMonster/Assets/1. Scripts/UI/Buttons/ChangeButtonSprite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VDFramework;
+
+namespace UI.Buttons
+{
+	public class ChangeButtonSprite : BetterMonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	{
+		[SerializeField]
+		private Sprite btnDown;
+
+		[SerializeField]
+		private Sprite btnUp;
+		
+		private Button button;
+
+		private void Start()
+		{
+			button              = GetComponent<Button>();
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			button.image.sprite = btnDown;
+			button.transform.Translate(Vector3.down * 8f);
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			button.image.sprite = btnUp;
+			button.transform.Translate(Vector3.up * 8f);
+		}
+	}
+}
