090ac4ac607bc9f5873b21efca23d916c1e65773
Danny-vD <dvduist@hotmail.nl>
2020-11-23 17:49:40 +0100

    Make a basis for so called 'extension screens' for the market

---
+++ LurkingMonster/Assets/1. Scripts/UI/Market/ExtensionScreens/AbstractMarketExtension.cs
@@ -0,0 +1,18 @@
+﻿using Grid.Tiles.Buildings;
+using UI.Market.MarketScreens;
+using UnityEngine;
+using VDFramework;
+
+namespace UI.Market.ExtensionScreens
+{
+	[RequireComponent(typeof(AbstractMarketScreen))]
+	public abstract class AbstractMarketExtension : BetterMonoBehaviour
+	{
+		private void Awake()
+		{
+			GetComponent<AbstractMarketScreen>().Activate += ActivateExtension;
+		}
+		
+		protected abstract void ActivateExtension(AbstractBuildingTile tile, MarketManager manager);
+	}
+}
