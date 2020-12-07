37635092e41f05c11b6f6555c685626e29a9719f
NiekTempert <niektempert@gmail.com>
2020-12-07 15:48:37 +0100

    Started with inputTutorial

---
+++ LurkingMonster/Assets/1. Scripts/Tutorial/InputTutorial.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _1._Scripts.Tutorial
+{
+	public class InputTutorial : global::Tutorial.Tutorial
+	{
+		private TMP_InputField inputText;
+		
+		
+
+		public override void StartTutorial(GameObject narrator, GameObject arrow)
+		{
+			base.StartTutorial(narrator, arrow);
+			
+			arrow.gameObject.SetActive(false);
+			narrator.gameObject.SetActive(false);
+			
+			inputText = GetComponent<TMP_InputField>();
+			inputText.gameObject.SetActive(true);
+			SetNextButton(CompleteTutorial);
+			inputText.onEndEdit.AddListener(SaveText);
+		}
+
+		private void SaveText(string input)
+		{
+			arrow.gameObject.SetActive(true);
+		}
+
+		private void CompleteTutorial()
+		{
+			TutorialManager.Instance.CompletedTutorial();
+			inputText.gameObject.SetActive(false);
+		}
+	}
+}
