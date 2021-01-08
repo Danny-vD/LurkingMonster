8c1aee1fcd41b2574bdf4ff411df9faaac377e7d
NiekTempert <niektempert@gmail.com>
2021-01-08 15:10:49 +0100

    Fixed bouncing stuff

--- LurkingMonster/Assets/1. Scripts/UI/TextLabels/AchievementCounter.cs
+++ LurkingMonster/Assets/1. Scripts/UI/TextLabels/AchievementCounter.cs
@@ -1,6 +1,7 @@
 ﻿using Events.Achievements;
 using Singletons;
 using TMPro;
+using UnityEngine;
 using VDFramework;
 using VDFramework.EventSystem;
 
@@ -8,12 +9,16 @@ namespace UI.TextLabels
 {
 	public class AchievementCounter : BetterMonoBehaviour
 	{
+		private GameObject parent;
 		private TextMeshProUGUI text;
 		
 		private void Start()
 		{
 			text = GetComponent<TextMeshProUGUI>();
 			EventManager.Instance.AddListener<AchievementUnlockedEvent>(ChangeText);
+			parent = transform.parent.gameObject;
+			
+			parent.SetActive(false);
 			
 			if (UserSettings.SettingsExist)
 			{
@@ -24,6 +29,7 @@ namespace UI.TextLabels
 		private void ChangeText()
 		{
 			text.text = RewardManager.Instance.Counter.ToString();
+			parent.SetActive(int.Parse(text.text) != 0);
 		}
 	}
 }
