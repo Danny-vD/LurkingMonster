00d7ba4085e8094d4c8219ec70786a4e8d7c4293
NiekTempert <niektempert@gmail.com>
2021-01-11 12:25:30 +0100

    Added rentPopupPrefab for collecting rent

---
+++ LurkingMonster/Assets/1. Scripts/Singletons/RentPopupManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Events.MoneyManagement;
+using UI.TextLabels;
+using UnityEngine;
+using VDFramework;
+using VDFramework.EventSystem;
+
+namespace Singletons
+{
+	public class RentPopupManager : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private GameObject rentPopupPrefab;
+
+		[SerializeField]
+		private Transform rentPopupParent;
+
+		private void Start()
+		{
+			EventManager.Instance.AddListener<CollectRentEvent>(DisplayRentPopup);
+		}
+
+		private void DisplayRentPopup(CollectRentEvent rent)
+		{
+			GameObject rentPopup = Instantiate(rentPopupPrefab, rentPopupParent);
+			rentPopup.GetComponent<MoneyJump>().SetUp(rent.Rent);
+		}
+	}
+}
--- LurkingMonster/Assets/1. Scripts/UI/TextLabels/MoneyJump.cs
+++ LurkingMonster/Assets/1. Scripts/UI/TextLabels/MoneyJump.cs
@@ -1,9 +1,45 @@
 ﻿using Events.MoneyManagement;
-using UI.Bounce;
+using FMOD;
+using TMPro;
+using UnityEngine;
+using VDFramework;
+using VDFramework.EventSystem;
 
 namespace UI.TextLabels
 {
-	public class MoneyJump : AbstractBounce<CollectRentEvent>
+	public class MoneyJump : BetterMonoBehaviour
 	{
+		private TextMeshProUGUI moneyText;
+		private Color textColor; //Change the alpha to fade out
+		
+		private float disappearTimer = 0.8f;
+		private float fadeOutSpeed = 1.5f;
+		private float moveSpeed = 12f;
+
+		
+		public void SetUp(int rent)
+		{
+			moneyText = GetComponent<TextMeshProUGUI>();
+			textColor = moneyText.color;
+			moneyText.SetText(rent.ToString());
+		}
+
+		private void LateUpdate()
+		{
+			transform.position += new Vector3(0f, moveSpeed * Time.deltaTime, 0f);
+			
+			disappearTimer     -= Time.deltaTime;
+			
+			if (disappearTimer <= 0f)
+			{
+				textColor.a     -= fadeOutSpeed * Time.deltaTime;
+				moneyText.color =  textColor;
+
+				if (textColor.a < 0f)
+				{
+					Destroy(gameObject);
+				}
+			}
+		}
 	}
 }
