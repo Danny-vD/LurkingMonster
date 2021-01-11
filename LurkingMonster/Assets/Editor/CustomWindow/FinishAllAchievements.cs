67875090694613a031858cea0061db926e57b726
NiekTempert <niektempert@gmail.com>
2021-01-11 14:13:48 +0100

    Removed singleton from TutorialManager so we can use it multiple times

---
+++ LurkingMonster/Assets/Editor/CustomWindow/FinishAllAchievements.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Singletons;
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomWindow
+{
+	public class RemoveSaveWindow : EditorWindow
+	{
+		[MenuItem("SaveGame/Delete")]
+		public static void ShowWindow()
+		{
+			GetWindow<RemoveSaveWindow>("Remove Save");
+		}
+
+		private void OnGUI()
+		{
+			if (UserSettings.SettingsExist)
+			{
+				if (!GUILayout.Button("Delete Save", EditorStyles.miniButtonMid)) return;
+
+				File.Delete(UserSettings.SavePath);
+			}
+			else
+			{
+				GUILayout.Label("No savefile found at " + UserSettings.SavePath);
+			}
+		}
+	}
+}
